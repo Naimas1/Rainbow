ee1f2869bcb4f1e4ae81883ef0db315eb5e08f3f
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-08 11:35:24 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Rainbow
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainbow
 {
-    internal class Program
+    class Programq
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = { 7, 14, 21, 28, 35, 42, 49, 56, 63, 70 };
+
+            Func<int[], int> countMultiplesOfSeven = arr => arr.Count(num => num % 7 == 0);
+
+            int result = countMultiplesOfSeven(numbers);
+            Console.WriteLine($"Кількість чисел, кратних семи: {result}");
         }
     }
 }
