382aa074361320cdf44048029e64ef2ec77120d7
Naimas1 <114872107+Naimas1@users.noreply.github.com>
2023-07-08 11:38:33 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Rainbow
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rainbow
 {
-    internal class Program
+    class Programw
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int[] numbers = { -2, 5, -10, 0, 15, -3, 8, 1 };
+
+            Func<int[], int> countPositiveNumbers = arr =>
+            {
+                int count = 0;
+                foreach (int num in arr)
+                {
+                    if (num > 0)
+                        count++;
+                }
+                return count;
+            };
+
+            int result = countPositiveNumbers(numbers);
+            Console.WriteLine($"Кількість позитивних чисел: {result}");
         }
     }
 }
